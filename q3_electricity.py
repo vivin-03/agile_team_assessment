db0a78c9c83b70aecddee838f7bcda575c64c2d8
vivin <vivinchowdary2005@gmail.com>
2026-01-08 14:46:58 +0530

    Add electricity bill program for Q3

---
+++ q3_electricity.py
@@ -0,0 +1,11 @@
+units = int(input("Enter units consumed: "))
+
+bill = 0
+if units <= 100:
+    bill = units * 2
+elif units <= 200:
+    bill = 100 * 2 + (units - 100) * 3
+else:
+    bill = 100 * 2 + 100 * 3 + (units - 200) * 5
+
+print("Total bill: ₹", bill)
